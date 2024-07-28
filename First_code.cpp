6ad7c4d88a89dbfc1e1641c83f223a06d3c56eda
Mayurijire1525 <Mayurijire441@gmail.com>
2024-07-28 12:26:10 +0530

    first Code

---
+++ First_code.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"----------Operations--------"<<endl;
+	int a,b,c;
+	cout<<"Enter no1 no2 "<<endl;
+	cin>>a;
+	cin>>b;
+	
+	c=a+b;
+	cout<<c<<endl;
+	cout<<"c="<<c<<endl;
+	cout<<"Add="<<c<<endl;
+	cout<<a<<"+"<<b<<"="<<c<<endl;
+	
+
+}
